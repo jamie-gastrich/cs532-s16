30d230b386e6ec1ccc4ac8a556ebd4b0709673d5
jamie-gastrich <jamie.gastrich@gmail.com>
2016-04-07 23:10:02 -0400

    Added files via upload

---
+++ A8_jgastric/q4/makeMDS.py
@@ -0,0 +1,15 @@
+# all code here stolen shamelessly from 
+# "Programming Collective Intelligence, Chapter 3"
+
+import sys
+
+sys.path.insert(0, '../libs')
+
+import clusters
+
+blognames,words,data=clusters.readfile('blogdata1.txt')
+
+coords = clusters.scaledown(data)
+
+clusters.draw2d(coords, blognames, jpeg='blogs2d.jpg')
+
